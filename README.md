4ccbeb2c4a1c10400932e7f830b71fa7b0b8cc60
isbhargav <ladbhargav@yahoo.co.in>
2021-01-06 17:16:25 -0500

    updated readable stream

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # stream-processing
- Working with Stream with Nodejs
+ Exmaples and Notes on working with Stream with Nodejs
+
+
+# Readable stream
+
+
+Readable streams are source of data
+Methods on readable streams
+- `.pipe()`
+- `.on('end',)`
+
+You dont need to call these often
+.read()
+.on('readable', () => { }) 
+
+`'readable'` is recommended as compared to `'data'` because readable is Pull based where as data is push based.
+
+you can let a module like `'pipeline'`or `.pipe()` take care of calling those. It is not advised to use pipe instead pipeline should used as it takes care if stripping down the streams in case of error to avoid any memory leaks.
+
+You can also use new async genrator to process a readable stream.
+
+Link to file : [rstream.js]('./../rstream.js')
+
--- rstream.js
+++ rstream.js
@@ -4,21 +4,40 @@
 // .pipe()
 // .on('end',)
 
-// You dont need to call these often 
+// You dont need to call these often
 
 // .read()
-// .on('readable', () => { })
+// .on('readable', () => { }) //PULL BASED
 
 // you can let a module or `pipe()` take care of calling those
 
+const fs = require("fs");
+const { argv } = require("process");
+const { pipeline } = require("stream");
+const stream = fs.createReadStream(argv[2] || argv[1], { encoding: "utf-8" });
 
-const fs = require('fs')
-const { argv } = require('process')
-const stream = fs.createReadStream(argv[2] || argv[1])
 
-stream.pipe(process.stdout)
 
-// Read streams can oprate in 2 modes 
+// stream.pipe(process.stdout);  - OLD Way, Dont's use this as it creates memory leaks
+
+// pipeline(stream, process.stdout, (err) => {
+//   console.log(err);
+// });
+
+// Every readable stream is asynchronously iterable
+async function logChunks(readable) {
+  let result = "";
+  for await (const chunk of readable) {
+    // console.log(chunk);
+    result += chunk;
+  }
+  return result;
+}
+
+
+logChunks(stream).then((result) => console.log(result));
+
+// Read streams can oprate in 2 modes
 // 1. Paused Mode (Defautl) - push only when called (automatic backpressure )
 // 2. Flow Mode  - continuesly pushing data
 //      - Turn Flow mode with stream.resume()
